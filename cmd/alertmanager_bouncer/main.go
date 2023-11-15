5fb77ef15243846b322b034249e5a527926f3656
sinkingpoint <colin@quirl.co.nz>
2023-11-15 11:09:19 +1100

    move to kong
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- cmd/alertmanager_bouncer/main.go
+++ cmd/alertmanager_bouncer/main.go
@@ -12,20 +12,20 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/alecthomas/kong"
 	"github.com/sinkingpoint/alertmanager_bouncer/lib/bouncer"
-	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-type config struct {
-	backendURL         *url.URL
-	listenURL          *net.TCPAddr
-	tlsCertFile        string
-	tlsKeyFile         string
-	bouncersConfigFile string
+var config struct {
+	BackendURL         *url.URL     `name:"backend.addr" help:"The URL of the backend to upstream to"`
+	ListenURL          *net.TCPAddr `name:"listen.addr" help:"The URL for the reverse proxy to listen on"`
+	TlsCertFile        string       `name:"tls.certfile" help:"The file path of the TLS cert file on disk, if you want to serve TLS"`
+	TlsKeyFile         string       `name:"tls.keyfile" help:"The file path of the TLS key file on disk, if you want to serve TLS"`
+	BouncersConfigFile string       `name:"config" help:"The file containing the list of bouncers to create"`
 }
 
-func loadBouncersFromFile(conf config) ([]bouncer.Bouncer, error) {
-	jsonFile, err := os.Open(conf.bouncersConfigFile)
+func loadBouncersFromFile() ([]bouncer.Bouncer, error) {
+	jsonFile, err := os.Open(config.BouncersConfigFile)
 	if err != nil {
 		return nil, err
 	}
@@ -40,29 +40,22 @@ func loadBouncersFromFile(conf config) ([]bouncer.Bouncer, error) {
 }
 
 func main() {
-	config := config{}
-	app := kingpin.New("alertmanager_bouncer", "A Business Logic Reverse Proxy for Alertmanager")
-	app.Flag("backend.addr", "The URL of the backend to upstream to").Required().URLVar(&config.backendURL)
-	app.Flag("listen.addr", "The URL for the reverse proxy to listen on").Required().TCPVar(&config.listenURL)
-	app.Flag("config.bouncersfile", "The file containing the list of bouncers to create").Required().ExistingFileVar(&config.bouncersConfigFile)
-	app.Flag("tls.certfile", "The file path of the TLS cert file on disk, if you want to serve TLS").ExistingFileVar(&config.tlsCertFile)
-	app.Flag("tls.keyfile", "The file path of the TLS key file on disk, if you want to serve TLS").ExistingFileVar(&config.tlsKeyFile)
-	kingpin.MustParse(app.Parse(os.Args[1:]))
+	kong.Parse(&config)
 
 	var err error
-	bouncers, err := loadBouncersFromFile(config)
+	bouncers, err := loadBouncersFromFile()
 	if err != nil {
-		log.Panicf("Failed to parse bouncers from %s: %s", config.bouncersConfigFile, err.Error())
+		log.Panicf("Failed to parse bouncers from %s: %s", config.BouncersConfigFile, err.Error())
 	}
 
 	fmt.Printf("Loaded %d bouncers\n", len(bouncers))
 
-	proxy := bouncer.NewBouncingReverseProxy(config.backendURL, bouncers, nil)
+	proxy := bouncer.NewBouncingReverseProxy(config.BackendURL, bouncers, nil)
 	server := http.Server{
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		Handler:      proxy,
-		Addr:         config.listenURL.String(),
+		Addr:         config.ListenURL.String(),
 	}
 
 	sigChan := make(chan os.Signal, 1)
@@ -70,22 +63,22 @@ func main() {
 	go func() {
 		for {
 			<-sigChan
-			log.Printf("Received a SIGHUP. Reloading Bouncers from %s", config.bouncersConfigFile)
-			bouncers, err := loadBouncersFromFile(config)
+			log.Printf("Received a SIGHUP. Reloading Bouncers from %s", config.BouncersConfigFile)
+			bouncers, err := loadBouncersFromFile()
 			if err != nil {
-				log.Printf("Failed to parse bouncers from %s: %s. Aboring Reload.", config.bouncersConfigFile, err.Error())
+				log.Printf("Failed to parse bouncers from %s: %s. Aboring Reload.", config.BouncersConfigFile, err.Error())
 			}
 
 			bouncer.SetBouncers(bouncers, proxy)
 		}
 	}()
 
-	if config.tlsCertFile != "" && config.tlsKeyFile != "" {
-		err = server.ListenAndServeTLS(config.tlsCertFile, config.tlsKeyFile)
+	if config.TlsCertFile != "" && config.TlsKeyFile != "" {
+		err = server.ListenAndServeTLS(config.TlsCertFile, config.TlsKeyFile)
 	} else {
-		if config.tlsCertFile != "" {
+		if config.TlsCertFile != "" {
 			log.Fatalln("TLS Cert file given without TLS Key File. Bailing.")
-		} else if config.tlsKeyFile != "" {
+		} else if config.TlsKeyFile != "" {
 			log.Fatalln("TLS Key file given without TLS Config File. Bailing.")
 		}
 		err = server.ListenAndServe()
--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module github.com/sinkingpoint/alertmanager_bouncer
 go 1.13
 
 require (
-	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
-	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
-	gopkg.in/alecthomas/kingpin.v2 v2.2.6
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/alecthomas/kong v0.8.1
 	gopkg.in/yaml.v3 v3.0.1
 )
